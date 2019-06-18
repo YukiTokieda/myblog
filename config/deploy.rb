84179751cca396af837b29295aebbcdbeb029b85
yuki tokieda <tokki7127@icloud.com>
2019-06-18 13:08:33 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['ssh/gptyn7127.pem']
+                  keys: ['~/.ssh/gptyn7127.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
